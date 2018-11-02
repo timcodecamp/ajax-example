e5cf776dc6e603885ebcecd6e326b878939dadc5
timcodecamp <44676243+timcodecamp@users.noreply.github.com>
2018-11-01 20:40:28 -0700

    upload ajax sample files
    uploading ajax sample files

---
+++ js/main.js
@@ -0,0 +1,57 @@
+var pageCounter = 1;
+var animalContainer = document.getElementById("animal-info");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click",function(){
+
+	var ourRequest = new XMLHttpRequest();
+	ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json'); // use open() to choose the method you want, if you want to get data, use GET, is you want to send data to server use POST
+
+	// do something when the data is loaded
+	ourRequest.onload = function() {
+		
+		if(ourRequest.status >= 200 && ourRequest.status < 400) {
+			var ourData = JSON.parse(ourRequest.responseText); //JSON.parse is to let the browser to read it as a JSON file
+			renderHTML(ourData);
+		}else {
+			alert("we connected the data but it returned an error");
+		}
+	};
+	ourRequest.send();
+	pageCounter ++;
+	if(pageCounter > 3) {
+		 btn.classList.add("hide-me");
+	}
+
+});
+
+function renderHTML(data) {
+	var htmlString = "";
+
+	for(i=0; i < data.length ; i++) {
+		htmlString += "<p>" + data[i].name + "is a " + data[i].species + " that likes to eat " ;
+
+		for(ii=0; ii < data[i].foods.likes.length; ii++) {
+			if(ii==0) {
+				htmlString += data[i].foods.likes[ii];
+			}else {
+				htmlString += " and " + data[i].foods.likes[ii];
+			}
+		}
+
+		htmlString += "and dislikes";
+		for(ii=0; ii < data[i].foods.dislikes.length; ii++) {
+			if(ii==0) {
+				htmlString += data[i].foods.dislikes[ii];
+			}else {
+				htmlString += " and " + data[i].foods.dislikes[ii];
+			}
+		}
+
+
+		htmlString += ".</p>";
+	}
+ 
+	animalContainer.insertAdjacentHTML("beforeend", htmlString)
+}
+
